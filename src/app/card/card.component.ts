368ee2c99778031c56615e601be2fd6d1faaa885
pyavchik <pyavchik@gmail.com>
2020-01-28 07:48:54 +0200

    tied data to a component

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-card',
@@ -7,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CardComponent implements OnInit {
 
+  @Input() title = '';
+  @Input() imageUrl = '';
+  @Input() username = '';
+  @Input() content = '';
   constructor() { }
 
   ngOnInit() {
